e0223357f8f8e489e6f6512dcd44be549b509328
João Vitor Andrade de Araujo <joao.araujo_ext@interplayers.com.br>
2022-11-14 12:11:05 -0300

    test

---
+++ index.js
@@ -0,0 +1,9 @@
+export let options = {
+    ext: {
+      loadimpact: {
+        projectID: 3609106,
+        // Test runs with the same name groups test runs together
+        name: "Project test"
+      }
+    }
+  }
--- src/tests/fakerApi/realizar_um_post_com_payload_recuperado.js
+++ src/tests/fakerApi/realizar_um_post_com_payload_recuperado.js
@@ -1,5 +1,13 @@
-import { Trend, Rate, Counter } from "k6/metrics";
-import { check, fail, sleep } from "k6";
+import {
+    Trend,
+    Rate,
+    Counter
+} from "k6/metrics";
+import {
+    check,
+    fail,
+    sleep
+} from "k6";
 import http from 'k6/http';
 
 import DevRequest from '../../support/requests/fakerApi/DevRequest.js';
@@ -7,17 +15,36 @@ import payloadExample from '../../fixtures/post.js'
 
 
 export const options = {
-    stages: [
-        { duration: '20s', target: 10 },
-        { duration: '11s', target: 60 },
-        { duration: '13s', target: 5 },
-        { duration: '2s', target: 15 },
+    stages: [{
+            duration: '20s',
+            target: 10
+        },
+        {
+            duration: '11s',
+            target: 60
+        },
+        {
+            duration: '13s',
+            target: 5
+        },
+        {
+            duration: '2s',
+            target: 15
+        },
     ],
 
     thresholds: {
         http_req_failed: ['rate<0.01'], // http errors should be less than 1%
         http_req_duration: ['p(95)<300'], // 95% of requests should be below 300ms
     },
+
+    ext: {
+        loadimpact: {
+            projectID: 3609106,
+            // Test runs with the same name groups test runs together
+            name: "Project test"
+        }
+    }
 }
 
 
@@ -32,7 +59,7 @@ export default () => {
 
     const params = {
         headers: {
-          'Content-Type': 'application/json; charset=utf-8; v=1.0',
+            'Content-Type': 'application/json; charset=utf-8; v=1.0',
         },
     };
 
@@ -45,5 +72,7 @@ export default () => {
     console.log("PAYLOAD AQUI: " + getPayload);
     console.log("STATUS AQUI: " + sendPayload.status);
 
-    check(sendPayload, { "Status deve ser igual a 200": r => r.status === 200 });
+    check(sendPayload, {
+        "Status deve ser igual a 200": r => r.status === 200
+    });
 }
