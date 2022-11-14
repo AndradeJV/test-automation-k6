850a5bb03b260109343b463f57a904729b5c295c
João Vitor Andrade de Araujo <joao.araujo_ext@interplayers.com.br>
2022-11-14 12:14:57 -0300

    test

--- index.js
+++
@@ -1,9 +0,0 @@
-export let options = {
-    ext: {
-      loadimpact: {
-        projectID: 3609106,
-        // Test runs with the same name groups test runs together
-        name: "Project test"
-      }
-    }
-  }
